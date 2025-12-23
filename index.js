6918f2ea9d5db8cbf33f62bae20f39a8bf536ca7
Bramara Tejaswini <bramaratejaswini09@gmail.com>
2025-12-23 15:48:29 +0530

    adding javascript

---
+++ index.js
@@ -0,0 +1,6 @@
+document.querySelector(".btn").addEventListener("click", function () {
+  const email = document.getElementById("email-input").value;
+  const password = document.getElementById("password").value;
+  console.log(email);
+  console.log(password);
+});
